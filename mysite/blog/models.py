720b6c291a35ef9f607c7f04b191d22857bfe444
Carlos Henrique <carlospaisca@gmail.com>
2016-07-07 19:54:08 -0300

    pagina 28

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                     .filter(status='published')
 
 
 class Post(models.Model):
@@ -21,6 +28,15 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 
 
 class Meta:
@@ -29,16 +45,3 @@ class Meta:
 
 def __str_(self):
     return self.title
-
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset()\
-                     .filter(status='published')
-
-
-class Post (models.Model):
-
-objects = models.Manager()
-
-published = PublishedManager()
---
+++ mysite/blog/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+from .import views
+from django.contrib import admin
+
+urlpatterns = [
+    # post views
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+        r'(?P<post>[-\w]+)/$',
+        views.post_detail,
+        name='post_detail'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls',
+                           namespace='blog',
+                           app_name='blog')),
+]
