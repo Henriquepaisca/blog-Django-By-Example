94802d561c53b4f73985ad91098e792575ab3ddd
Carlos Henrique <carlospaisca@gmail.com>
2016-07-19 15:58:02 -0300

    lalalala blog

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
         r'(?P<post>[-\w]+)/$',
         views.post_detail,
-        name='post_detail'),
-        url(r'^new/$', views.post_new, name='post_new'),
+        name='post_detail'), url(r'^new/$', views.post_new, name='post_new'),
 ]
